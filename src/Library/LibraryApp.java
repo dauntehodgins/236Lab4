0edfe188cd0506d5ac2925e43100dc41279931d9
aHopfner <151684719+aHopfner@users.noreply.github.com>
2024-10-22 18:54:47 -0700

    Update LibraryApp.java
    fucked it

--- src/Library/LibraryApp.java
+++ src/Library/LibraryApp.java
@@ -2,12 +2,6 @@ package Library;
 
 public class LibraryApp {
     public static void main(String[] args) {
-    	LibrarianController controller = new LibrarianController();
-    	Library lib = new Library();
-    	
-    	//Add members
-    	lib.createMember("Alex");
-    	lib.createMember("Daunte");
-    	lib.createMember("Gursheen");
+ 
     }
 }
